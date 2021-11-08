32659f996b2d3cd2a1d06670f6c55a7eb4f1575d
Елена <lissik21@tandex.ru>
2021-11-08 17:29:38 +0300

    домашнее задание 1

--- src/main/java/lesson1/FirstProgram.java
+++ src/main/java/lesson1/FirstProgram.java
@@ -2,6 +2,7 @@ package lesson1;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("01");
+
+        System.out.println("Привет,мир!");
     }
 }
