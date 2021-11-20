328dd4aabc74644aa78738d5ab6af690868933fe
Елена <lissik21@tandex.ru>
2021-11-20 10:41:33 +0300

    домашнее задание

--- src/main/java/lesson2/SecondProgram.java
+++ src/main/java/lesson2/SecondProgram.java
@@ -4,19 +4,32 @@ public class SecondProgram<resultOfMinus> {
 
     public static void main(String[] args) {
         //сложение
-        long summa = summa(3, 5);
-        long resultOfMinus = minus(18, 2);
-        System.out.println(resultOfMinus);
+        long summa = summa(3,5);
+        long minus = minus(18,2);
+        long prod = prod(5,5);
+        long div = div(18,2);
+        System.out.println(minus);
+        System.out.println(summa);
+        System.out.println(prod);
+        System.out.println(div);
     }
-
-    public static long summa(int arg1, int arg2) {
+        public static long summa(int arg1, int arg2) {
         int result = arg1 + arg2;
-        System.out.println(result);
         return result;
     }
 
     public static long minus(int arg1, int arg2) {
-        int result = arg1 - arg2;
+        int result= arg1- arg2;
+        return result;
+
+    }
+    public static long prod(int arg1, int arg2) {
+        int result = arg1 * arg2;
+        return result;
+    }
+
+    public static long div(int arg1, int arg2) {
+        int result = arg1 / arg2;
         return result;
 
     }
